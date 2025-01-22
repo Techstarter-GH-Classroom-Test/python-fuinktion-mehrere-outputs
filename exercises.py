cce630cb0f9db9546e1fe2868f9efc0ff1afc467
maximilian-techstarter <maximilian.keil@techstarter.de>
2025-01-22 14:51:16 +0100

    first commit

--- exercises.py
+++ exercises.py
@@ -1,14 +1,9 @@
-## Template Aufgabenbeschreibung
-# Aufgabe 1: Schreibe ein Programm, das eine personalisierte Begrüßung ausgibt.
-# 
-# 1. Lese einen Namen von der Konsole ein.
-# 2. Gib eine personalisierte Begrüßung aus, z. B.: "Hallo, [Name]!".
-# 
-# Hinweise (optional):
-# - Verwende die Funktion `input()`, um Benutzereingaben einzulesen.
-# - Verwende die Funktion `print()`, um die Ausgabe zu erzeugen.
-#
-# Beispiel (optional):
-# Wenn die Eingabe "Alice" ist, sollte die Ausgabe "Hallo, Alice!" sein.
-#
-# Viel Erfolg!
+# Aufgabe 1: Schreibe eine Funktion mit dem Namen calc_1()
+# die den Durchschnitt und die Anzahl der Elemente einers Arrays mit Zahlen mit return wieder gibt.
+def calc_1(array):
+    return 0
+
+# Aufgabe 2: Schreibe eine Funktion mit dem Namen calc_2()
+# die zwei Parameter: n und m engegen nimmt mit return n + m und n * m wieder gibt
+def calc_2(n, m):
+    return 0
